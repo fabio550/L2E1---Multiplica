81ebaa7fa44a0d27b6b0faea4219e7fe50ddabfe
fabio <fmoreira550@gmail.com>
2023-08-14 22:00:11 -0300

    JOptionPanel adicionado

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,11 +1,18 @@
 package view;
 import controller.MultiplicaController;
 
+import javax.swing.*;
+
 public class Principal {
     public static void main(String[] args) {
+
         MultiplicaController mc = new MultiplicaController();
 
-        int res = mc.CalcMultiplica(6,5);
-        System.out.println(res);
+        int a = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor a:"));
+        int b = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor b:"));
+
+        int res = mc.CalcMultiplica(a,b);
+
+        JOptionPane.showMessageDialog(null, "O resultado da multiplicação de " + a + " por " + b + " é " + res); // 20 24 -> 4
     }
 }
